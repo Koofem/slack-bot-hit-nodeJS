a6442eabaee4fa1a36ce90cdc9ee24c7f47d0358
mike <Libroks@yandex.ru>
2021-07-20 19:59:48 +0300

    Бот для hit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const schedule = require('node-schedule');
 dotenv.config();
 
 const rule = new schedule.RecurrenceRule();
-rule.dayOfWeek = [new schedule.Range(1, 5)];
+rule.dayOfWeek = [new schedule.Range(1, 6)];
 rule.hour = 9;
 rule.minute = 0;
 
@@ -16,13 +16,12 @@ const bot = new SlackBot({
 })
 
 
-bot.on('start', () => { 
-    bot.postMessageToChannel(
-        'hit-dsm',
-        'Тест бота'
-    );  
+bot.on('start', () => {
     const job = schedule.scheduleJob(rule, function(){
-
+        bot.postMessageToChannel(
+            'hit-dsm',
+            '\n Доброе утро, команда мечты! :sunny: \n<!here>\n1. Что вы сделали вчера? \n2. Что планируете сегодня? \n3. Есть какие-нибудь вопросы/блокеры? \n'
+        );  
       });
 })
 
